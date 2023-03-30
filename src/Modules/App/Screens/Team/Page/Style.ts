a06253de98b4470dd2fdc425293ac5bd790d1104
Guilherme Fialho <guilhermec.fialho@gmail.com>
2023-03-30 10:37:07 -0300

    Configuring project

--- src/Modules/App/Screens/Team/Page/Style.ts
+++ src/Modules/App/Screens/Team/Page/Style.ts
@@ -1,3 +1,11 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+import BackgroundSvg from "@/Assets/Background/background2.svg";
+
+export const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-image: url(${BackgroundSvg});
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+
+declare const __dirname: string;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: "@", replacement: "./src" }],
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
 });
