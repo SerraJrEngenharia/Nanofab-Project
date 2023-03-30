d7855c734db9bec0d3d102460bf01a4e25f21b20
Guilherme Fialho <guilhermec.fialho@gmail.com>
2023-03-30 09:10:43 -0300

    Creating all pages

--- src/Modules/App/Routes/Index.Routes.tsx
+++ src/Modules/App/Routes/Index.Routes.tsx
@@ -2,15 +2,15 @@
 import React from "react";
 
 // Router
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // Pages
 import HomePage from "@/Modules/App/Screens/Home/Page/Index";
+import TeamPage from "@/Modules/App/Screens/Team/Page/Index";
+import ProgressPage from "@/Modules/App/Screens/Progress/Page/Index";
+import ProjectsPage from "@/Modules/App/Screens/Projects/Page/Index";
+import PublicationsPage from "@/Modules/App/Screens/Publications/Page/Index";
+import PartnershipsPage from "@/Modules/App/Screens/Partnerships/Page/Index";
 import ErrorPage from "@/Modules/App/Screens/Error/Page/Index";
 
 // @Types
@@ -22,8 +22,24 @@ const router = createBrowserRouter([
     element: <HomePage />,
   },
   {
-    path: "about",
-    element: <HomePage />,
+    path: "/equipe",
+    element: <TeamPage />,
+  },
+  {
+    path: "/andamento",
+    element: <ProgressPage />,
+  },
+  {
+    path: "/submissao_projetos",
+    element: <ProjectsPage />,
+  },
+  {
+    path: "/publicacoes",
+    element: <PublicationsPage />,
+  },
+  {
+    path: "/parcerias",
+    element: <PartnershipsPage />,
   },
   {
     path: "*",
---
+++ src/Modules/App/Screens/Team/Page/Index.tsx
@@ -0,0 +1,24 @@
+// React
+import React from "react";
+
+// Native Components
+import { Container } from "./Style";
+
+// Templates
+import HeaderTemplate from "@/Templates/Header/Page/Index";
+import FooterTemplate from "@/Templates/Footer/Page/Index";
+
+// @Types
+import { PageProps } from "./Types";
+
+const TeamPage: React.FC<PageProps> = ({}) => {
+  return (
+    <Container>
+      <HeaderTemplate />
+
+      <FooterTemplate />
+    </Container>
+  );
+};
+
+export default TeamPage;
